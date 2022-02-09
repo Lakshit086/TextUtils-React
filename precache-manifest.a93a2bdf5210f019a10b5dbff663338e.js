a612def59a35a6026e75837028fe23983dfa0649
Lakshit086 <98867585+Lakshit086@users.noreply.github.com>
2022-02-09 12:18:20 +0530

    Updates

---
+++ precache-manifest.a93a2bdf5210f019a10b5dbff663338e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede5f15508f828ff5bd2247e6bab3b66",
+    "url": "/TextUtils-React/index.html"
+  },
+  {
+    "revision": "33e6debf4e8565358876",
+    "url": "/TextUtils-React/static/css/main.97605b0d.chunk.css"
+  },
+  {
+    "revision": "5da4d59910867ec6fb96",
+    "url": "/TextUtils-React/static/js/2.cd8ee3dc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/TextUtils-React/static/js/2.cd8ee3dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b47aee0090a49691be00",
+    "url": "/TextUtils-React/static/js/3.5b3a70da.chunk.js"
+  },
+  {
+    "revision": "33e6debf4e8565358876",
+    "url": "/TextUtils-React/static/js/main.43f6d99d.chunk.js"
+  },
+  {
+    "revision": "239a029f2d73b7b702d6",
+    "url": "/TextUtils-React/static/js/runtime-main.4e75fc89.js"
+  }
+]);
